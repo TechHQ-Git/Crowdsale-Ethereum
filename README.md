4bfcda1e74a0f901adcd27a9b6892dbfa0570916
Alberto Cuesta Cañada <mail@albertocuesta.es>
2018-06-14 12:32:18 +0100

    Initial implementation of Token contract

---
+++ README.md
@@ -0,0 +1,3 @@
+Implementation of the ethereum based crowdsale application tutorial at https://www.ethereum.org/crowdsale
+
+If nvm, node.js 8, npm:latest and remixd are installed you can use the start-remixd.sh script to integrate your local directory to the Remix IDE
---
+++ crowdfund.sol
@@ -0,0 +1,71 @@
+// To be compiled with compiler versions 0.4 <= v < 0.5
+pragma solidity ^0.4.0;
+
+contract Token {
+    /* Public variables of the token.*/
+    bool initialized = false;
+    address owner = msg.sender;
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    
+    /* This creates an array with all balances.*/
+    mapping(address => uint256) balances;
+    
+    /**
+     * @dev This generates a public event on the blockchain that will notify 
+     * clients of a token transfer.
+     * @param _from - The address transferring the tokens
+     * @param _to - The address receiving the tokens
+     * @param _amount - The amount of tokens to be transferred
+    */
+    event TokenTransfer(
+        address indexed _from, 
+        address indexed _to, 
+        uint256 indexed _amount
+        );
+    
+    /**
+     * @dev Initializes contract with initial supply tokens to the creator of 
+     * the token.
+     * @param _initialSupply - Tokens created with the contract
+     * @param _name - Token name (e.g. Bitcoin)
+     * @param _symbol - Token symbol (e.g. BTC)
+     * @param _decimals - Minimum token fraction (e.g. 0.001)
+     */
+    constructor (
+                uint256 _initialSupply,
+                string _name,
+                string _symbol,
+                uint8 _decimals
+            ) public {
+        assert(initialized == false);  // Check whether you could declare the variable here first
+        require(owner == msg.sender);
+        balances[msg.sender] = _initialSupply;
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        initialized = true;
+    }
+
+    /**
+     * @dev Send tokens. Note that all addresses exist, so it is possible to 
+     * send tokens to an address that no one has claimed yet, and that tokens 
+     * sent to an address for which no one knows the private key are basically 
+     * irrecoverable.
+     * @param _from - The address transferring the tokens
+     * @param _to - The address receiving the tokens
+     * @param _amount - The amount of tokens to be transferred
+     */
+    function transfer(
+                address _from, 
+                address _to, 
+                uint256 _amount
+            ) public {
+        require(_from == msg.sender);  // Check require vs throw
+        require(balances[_from] >= _amount);
+        balances[_from] = balances[_from] - _amount;  // Use SafeMath
+        balances[_to] = balances[_to] + _amount;  // Use SafeMath
+        emit TokenTransfer(_from, _to, _amount);
+    }
+}
