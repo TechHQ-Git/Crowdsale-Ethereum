e77821a3a7ca649fb89d5e8a3d5eed911a484c53
Alberto Cuesta Cañada <mail@albertocuesta.es>
2018-07-02 14:10:37 +0100

    Added test for transfer.

--- test/TestToken.sol
+++ test/TestToken.sol
@@ -7,13 +7,6 @@ import "../contracts/Token.sol";
 contract TestToken {
   Token token = Token(DeployedAddresses.Token());
 
-  uint expectedInitialSupply = 100;
-  uint8 expectedDecimals = 16;
-  // Arrays (including strings) and structs are implicit "storage", it seems.
-  string expectedName = "TestToken";
-  string expectedSymbol = "TTK";
-
-
   // Testing the constructor() function
   function testConstructor() public {
     address expected = msg.sender;
@@ -26,44 +19,59 @@ contract TestToken {
   }
 
   // Testing the initial supply through initialize() function
-  function testInitializeSupply() public {
+  function testInitialize() public {
+      uint expectedInitialSupply = 100;
+      uint8 expectedDecimals = 16;
+      // Strings are arrays and therefore storage by default, I think.
+      string memory expectedName = "TestToken";
+      string memory expectedSymbol = "TTK";
+
     token.initialize(
       expectedInitialSupply,
       expectedName,
       expectedSymbol,
       expectedDecimals
       );
+
     Assert.equal(
       expectedInitialSupply,
       uint256(token.getBalanceOf(this)), // Truffle needs to be told the type
       "The owner of the contract should have 100 TTK."
       );
-  }
 
-  // Testing the decimals through initialize() function
-  function testInitializeDecimals() public {
     Assert.equal(
       uint256(expectedDecimals), // TRUFFLE CAN'T TEST UINT8!!!
       uint256(token.decimals()),
       "The TestToken should have support for 16 decimals."
       );
-  }
 
-  // Testing the name through initialize() function
-  function testInitializeName() public {
     Assert.equal(
       expectedName,
       string(token.name()),
       "The name of the token should be \"TestToken\"."
       );
-  }
 
-  // Testing the symbol through initialize() function
-  function testInitializeSymbol() public {
     Assert.equal(
       expectedSymbol,
       string(token.symbol()),
       "The symbol of the token should be \"TTK\"."
+      );
+  }
+
+  // Testing the transfer of tokens from the owner to a random address.
+  // The address receiving the accounts is generated with ganache-cli using the
+  // seed "hello". Be careful of not testing this where you would burn the tokens
+  // Public: 0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1
+  // Truffle requires addresses to be checksummed with mixed upper/lowercase
+  // https://github.com/ethereum/EIPs/issues/55
+  function testTransfer() public {
+    address to = 0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1;
+    uint256 expectedBalance = 1;
+    token.transfer(to,1);
+    Assert.equal(
+      expectedBalance,
+      uint256(token.getBalanceOf(to)),
+      "The balance of 0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1 should be 1."
       );
   }
 }
